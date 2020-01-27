dcef495ec5037ab5272a08eded8c2eb6fb047930
taishi352 <taishi352@gmail.com>
2020-01-27 08:00:34 +0000

    finish 課題三 part2

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -6,6 +6,13 @@ class BookCommentsController < ApplicationController
 		@comment.save
 		redirect_back(fallback_location: root_path)
 	end
+	def destroy
+		@book = Book.find(params[:book_id])
+		@comment = current_user.book_comments.find_by(book_id: @book.id)
+		@comment.book_id = @book.id
+		@comment.destroy
+		redirect_back(fallback_location: root_path)
+	end
 
 	private
 	def book_comment_params
