34ea9dfcd2698742fb0f0cdef39356a98242be24
taishi352 <taishi352@gmail.com>
2020-01-28 05:30:03 +0000

    フォロー機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -7,9 +7,14 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :index, :edit, :update] do
   	get :about, on: :collection
+    member do
+      get :follows
+      get :followers
+    end
   end
   resources :books, only: [:show, :index, :create, :edit, :update, :destroy] do
   	resource :favorites, only: [:create, :destroy]
   	resource :book_comments, only: [:create, :destroy]
   end
+  resources :relationships, only: [:create, :destroy]
 end
